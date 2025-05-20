1e29b6fb80438401e8b2cbfbda76a963ed5ae88e
tangs <tangscna@gmail.com>
2025-05-20 22:04:31 +0800

    modify 调整错误Code

--- pkg/ierr/code.go
+++ pkg/ierr/code.go
@@ -14,25 +14,22 @@ var (
 	NotAllowed       Code = 403
 	BadRequest       Code = 400
 	TokenExpire      Code = 40001
-	PermissionDeined Code = 103 // 权限不足
+	PermissionDenied Code = 103 // 权限不足
+	NotFound         Code = 404
 
 	// 通用错误
 	InvalidId              Code = 10001 // 无效的id
 	InvalidState           Code = 10002 // 无效的状态
-	ParamErr               Code = 10003 //参数错误
+	ParamErr               Code = 10003 // 参数错误
 	ReceiveRequestFileFail Code = 10004
 	InvalidType            Code = 10005 // 无效的类型
 	NonPointError          Code = 10006 // 数据空指针
-	WriteFileError         Code = 10007 // 写入文件错误
-	ReadFileError          Code = 10008 // 读取文件错误
-	QueueWriteError        Code = 10009 // 写入队列失败
-	QueueReadError         Code = 10010 // 读取队列失败
-	UnexpectedError        Code = 10011 // 意外的错误
-	InternalError          Code = 10012 // 内部错误
-	InternalCallRequest    Code = 10013 // 内部调用错误
-	ThirdCallRequest       Code = 10014 // 外部调用错误
-	PhoneRepeatError       Code = 10015 // 电话号码重复
-	NetworkBusyError       Code = 10016 // 网络繁忙，请稍后再试
+	ParseDataFail          Code = 10007 // 解码失败
+	InternalError          Code = 10008 // 内部错误
+	InternalCallRequest    Code = 10009 // 内部调用错误
+	ConcurrentError        Code = 10010 // 并发错误
+	NetworkBusyError       Code = 10011 // 网络繁忙，请稍后再试
+	NetwordError           Code = 10012 // 网络错误
 
 	// 数据库相关
 	CreateDataFail    Code = 50001 // 创建数据失败
@@ -41,7 +38,8 @@ var (
 	QueryDataFail     Code = 50004 // 查询数据失败
 	CommitTxFail      Code = 50005 // 提交事务失败
 	CountDataFail     Code = 50006 // 统计数据失败
-	RedisOperatorFail Code = 50007 //redis操作数据失败
+	RedisOperatorFail Code = 50007 // redis操作数据失败
 	UpsertDataFail    Code = 50008 // upsert数据失败
-
+	ScanDataFail      Code = 50009 // scan数据
+	GroupByDataFail   Code = 50010 // groupBy数据失败
 )
