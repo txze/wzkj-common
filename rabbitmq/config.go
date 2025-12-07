71e2f1ae2a56b1f437d7f9a2f0f783857e7a0cf0
tangs <tangscna@gmail.com>
2025-12-07 21:27:20 +0800

    modify 调整初始化

--- rabbitmq/config.go
+++ rabbitmq/config.go
@@ -1,11 +1,5 @@
 package rabbitmq
 
-import (
-	"errors"
-
-	"glprivate.quanyougame.net/backend/core-sdk-go/nacos"
-)
-
 type RabbitMQConf struct {
 	Host string `json:"host"`
 	User string `json:"user"`
@@ -13,32 +7,10 @@ type RabbitMQConf struct {
 	TLS  bool   `json:"tls"`
 }
 
-func loadConfig() (*RabbitMQConf, error) {
-	var conf map[string]map[string]*RabbitMQConf
-	if err := nacos.ScanInfraConf(&conf); err != nil {
-		return nil, err
-	}
-	mqConf, ok := conf["mq"]
-	if !ok {
-		return nil, errors.New("mq config not found")
-	}
-	rmqConf, ok := mqConf["rabbitmq"]
-	if !ok {
-		return nil, errors.New("rabbitmq config not found")
-	}
-	return rmqConf, nil
-}
-
 // NewRabbitMQByInternalConf 直接读取nacos的配置，初始化rabbitmq.
-func NewRabbitMQByInternalConf(serverName string, opts ...Option) *RabbitMQ {
-	conf, err := loadConfig()
-	if err != nil {
-		panic("failed to load rabbitmq config: " + err.Error())
-	}
-	opts = append(opts, WithHost(conf.Host), WithUser(conf.User), WithPwd(conf.Pwd))
-	if !conf.TLS {
-		opts = append(opts, WithDisableTLS(true))
-	}
+func NewRabbitMQWithConfig(serverName string, host, user, pwd string) *RabbitMQ {
+	var opts []Option
+	opts = append(opts, WithHost(host), WithUser(user), WithPwd(pwd))
 	return NewRabbitMQ(
 		serverName,
 		opts...,
